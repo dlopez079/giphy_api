79e8540065ff8eafcf7ec93169d75becc9caa494
David Lopez <dlopez079@gmail.com>
2019-09-17 01:14:57 -0400

    Added bootstrap to file to begin designing

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,71 +1,17 @@
 //Create a console.log to confirm that the javascript file is connected. 
 console.log("This java file is linked");
 
-//BEGIN DOCUMENT READY!!!!!
-// $(document).ready(function() {
 
 //Create a var for API key
 var api_key = "lYDMPf6QY9UggTQK6AoI5J6nt6PrRcQQ";
 
-//var for user's input
-// userInput = 
 
 //Create an array based on a specific topic
 var teams = ["Atlanta Braves", "Washington Nationals", "New York Mets", "Philadelphia Phillies", "Miami Marlins"];
 
-
-//FUNCTION FOR DISPLAYING BUTTONS
-function renderbuttons() {
-
-  //Deleting the movie buttons prior to adding new movie buttons
-  $("#buttons-row").empty();
-
-  //loop through the array of movies
-  for (var i = 0; i < teams.length; i++) {
-
-    //call a variable that will create a button
-    var a = $("<button>");
-
-    //Add a class
-    a.addClass("team");
-
-    //Adding a data-attribute with a value of the team at index i
-    a.attr("data-team", teams[i]);
-
-    //Providing the buttons'text with a a value of the team at index i
-    a.text(teams[i]);
-    
-    //Add button to html
-    $("#buttons-row").append(a);
-  }
-
-}
-
-//Create a function for the team button click
-$("#add-team").on("click", function(event) {
-  
-  //prevent form from trying to submit itself
-  event.preventDefault();
-  
-  //Empty team input box
-  $("#team-input").empty();
-
-  //Grab the text from the input box
-  var team = $("#team-input").val().trim();
-
-  //Push the team input to the array
-  teams.push(team);
-
-  //call the render function the render the teams in the array
-  renderbuttons();
-  
-})
-
-//Call the renderbuttons function to list the original array.
-renderbuttons();
-
-
-$("button").on("click", function() {
+//CREATE FUNCTION TO DISPLAY GIFS USING AJAX
+function displayGifs() {
+  $("button").on("click", function () {
     var team = $(this).attr("data-team");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + api_key + "&q=" + team + "&limit=10&offset=0&rating=G&lang=en";
     console.log(queryURL);
@@ -74,9 +20,8 @@ $("button").on("click", function() {
       url: queryURL,
       method: "GET"
     })
-      .then(function(response) {
+      .then(function (response) {
         var results = response.data;
-        console.log(response);
 
         for (var i = 0; i < results.length; i++) {
           var gifDiv = $("<div>"); //creates a div for the gif results
@@ -95,6 +40,95 @@ $("button").on("click", function() {
         }
       });
   });
+} //END FUNCTION TO DISPLAY GIFS USING AJAX***********************************
+
+
+
+//FUNCTION FOR DISPLAYING BUTTONS IN ARRAY************************************
+function renderbuttons() {
+
+  //Deleting the movie buttons prior to adding new team buttons
+  $("#buttons-row").empty();
+
+  //loop through the array of movies
+  for (var i = 0; i < teams.length; i++) {
+
+    //call a variable that will create a button
+    var a = $("<button>");
+
+    //Add a class
+    a.addClass("team");
+
+    //Adding a data-attribute with a value of the team at index i
+    a.attr("data-team", teams[i]);
+
+    //Providing the buttons'text with a a value of the team at index i
+    a.text(teams[i]);
+
+    //Add button to html
+    $("#buttons-row").append(a);
+  } //END FUNCTION FOR DISPLAYING BUTTON IN ARRAY*******************************
+
+}
+
+//FUCTION TO ADD TEAM BUTTON*******************************************
+$("#add-team").on("click", function (event) {
+
+  //prevent form from trying to submit itself
+  event.preventDefault();
+
+  //Empty team input box
+  $("#team-input").empty();
+
+  //Grab the text from the input box
+  var team = $("#team-input").val().trim();
+
+  //Push the team input to the array
+  teams.push(team);
+
+  //call the render function the render the teams in the array
+  renderbuttons();
+
+}) //END FUNCTION TO ADD TEAM BUTTON************************************
+
+$(document).on("click", ".team", displayGifs);
+
+
+//Call the renderbuttons function to list the original array.
+renderbuttons();
+
+
+
+// $("button").on("click", function() {
+//     var team = $(this).attr("data-team");
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + api_key + "&q=" + team + "&limit=10&offset=0&rating=G&lang=en";
+//     console.log(queryURL);
+
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     })
+//       .then(function(response) {
+//         var results = response.data;
+//         console.log(response);
+
+//         for (var i = 0; i < results.length; i++) {
+//           var gifDiv = $("<div>"); //creates a div for the gif results
+
+//           var rating = results[i].rating; //setting a variable for ratings
+
+//           var p = $("<p>").text("Rating: " + rating);  //creating a paragraph that displays the text: "Rating: + results[i].rating"
+
+//           var teamImage = $("<img>"); //setting a variable for the creation of an image
+//           teamImage.attr("src", results[i].images.fixed_height.url); //setting an attribute for the teamImage.  We will use the URL from the database to pull gif. 
+
+//           gifDiv.prepend(p);  //This will create and prepend a paragraph onto the HTML.
+//           gifDiv.prepend(teamImage);//This will prepend the image/gif onto the HTML.
+
+//           $("#results-row").prepend(gifDiv);
+//         }
+//       });
+//   });
 
 
 // }); //END OF DOCUMENT READY!!!!
