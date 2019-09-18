ebda328b5a3b905fafdf711626928b6946538400
dlopez079 <dlopez079@gmail.com>
2019-09-18 09:52:58 -0400

    I removed the gif pause instructions out the function pauseGifs.  I used document on click and tagged .motion.   I added the class motion to each image.

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -11,7 +11,7 @@ var teams = ["Atlanta Braves", "Washington Nationals", "New York Mets", "Philade
 
 //CREATE FUNCTION TO DISPLAY GIFS USING AJAX
 function displayGifs() {
-  $("button").on("click", function () {
+  $(document).on("click", ".team", function () {
     var team = $(this).attr("data-team");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + api_key + "&q=" + team + "&limit=10&offset=0&rating=G&lang=en";
     console.log(queryURL);
@@ -26,14 +26,19 @@ function displayGifs() {
 
         for (var i = 0; i < results.length; i++) {
           var gifDiv = $("<div>"); //creates a div for the gif results
-          gifDiv.addClass("col");
-
+          gifDiv.addClass("gifs");
+          
           var rating = results[i].rating; //setting a variable for ratings
-
+          console.log(results[i]);
           var p = $("<p>").text("Rating: " + rating);  //creating a paragraph that displays the text: "Rating: + results[i].rating"
 
           var teamImage = $("<img>"); //setting a variable for the creation of an image
           teamImage.attr("src", results[i].images.fixed_height.url); //setting an attribute for the teamImage.  We will use the URL from the database to pull gif. 
+          
+          teamImage.attr("data-still", results[i].images.fixed_height_still.url);
+          teamImage.attr("data-animate", results[i].images.fixed_height.url);
+          teamImage.attr("data-state", "still");
+          teamImage.addClass("motion");
 
           gifDiv.prepend(p);  //This will create and prepend a paragraph onto the HTML.
           gifDiv.prepend(teamImage);//This will prepend the image/gif onto the HTML.
@@ -49,7 +54,7 @@ function displayGifs() {
 //FUNCTION FOR DISPLAYING BUTTONS IN ARRAY************************************
 function renderbuttons() {
 
-  //Deleting the movie buttons prior to adding new team buttons
+  //Deleting the team buttons prior to adding new team buttons
   $("#buttons-row").empty();
 
   //loop through the array of movies
@@ -102,38 +107,22 @@ $(document).on("click", ".team", displayGifs);
 //Call the renderbuttons function to list the original array.
 renderbuttons();
 
+//FUNCTION TO PAUSE GIFS**************************************************
 
+  $(document).on("click", ".motion", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {  
+      $(this).attr("src", $(this).attr("data-animate")); 
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+ //END OF FUNCTION TO PAUSE GIFS*****************************************
 
-// $("button").on("click", function() {
-//     var team = $(this).attr("data-team");
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + api_key + "&q=" + team + "&limit=10&offset=0&rating=G&lang=en";
-//     console.log(queryURL);
-
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     })
-//       .then(function(response) {
-//         var results = response.data;
-//         console.log(response);
-
-//         for (var i = 0; i < results.length; i++) {
-//           var gifDiv = $("<div>"); //creates a div for the gif results
-
-//           var rating = results[i].rating; //setting a variable for ratings
-
-//           var p = $("<p>").text("Rating: " + rating);  //creating a paragraph that displays the text: "Rating: + results[i].rating"
-
-//           var teamImage = $("<img>"); //setting a variable for the creation of an image
-//           teamImage.attr("src", results[i].images.fixed_height.url); //setting an attribute for the teamImage.  We will use the URL from the database to pull gif. 
-
-//           gifDiv.prepend(p);  //This will create and prepend a paragraph onto the HTML.
-//           gifDiv.prepend(teamImage);//This will prepend the image/gif onto the HTML.
-
-//           $("#results-row").prepend(gifDiv);
-//         }
-//       });
-//   });
-
-
-// }); //END OF DOCUMENT READY!!!!
+pauseGifs();
